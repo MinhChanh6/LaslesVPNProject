4b5f7eb0622a371e81af70bbf905528fc0da29b2
MinhChanh6 <hominhchanh08@gmail.com>
2021-05-01 18:20:25 +0700

    first-commit

---
+++ index.js
@@ -0,0 +1,45 @@
+$('.burger').on('click', function(event){
+    
+    $('.navbar__bar').slideToggle('200');
+})
+
+const str = ScrollReveal(
+    {
+        distance: '30px',
+        duration: 1800,
+        reset:true,
+    }
+);
+
+str.reveal(`.navbar`,{
+    origin:'top',
+    interval:200,
+})
+str.reveal(`.content__left`,{
+    origin:'right',
+    interval:300,
+})
+str.reveal(`.content__right`,{
+    origin:'left',
+    interval:300,
+})
+str.reveal(`.info__test`,{
+    origin:'bottom',
+    interval:500,
+})
+str.reveal(`.left`,{
+    origin:'right',
+    interval:300,
+})
+str.reveal(`.right`,{
+    origin:'left',
+    interval:300,
+})
+str.reveal(`.package__items`,{
+    origin:'top',
+    interval:200,
+})
+str.reveal(`.footer__content`,{
+    origin:'bottom',
+    interval:200,
+})
